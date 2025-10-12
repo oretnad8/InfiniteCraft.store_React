cab8bb0345b90c8dbf984ed5de9bc32140262a7e
oretnad8 <or.etnad8@gmail.com>
2025-10-12 11:38:23 -0300

    Implementacion de datos base de productos y regiones/comunas

---
+++ src/data/products.ts
@@ -0,0 +1,23 @@
+export const products = [
+    {
+        id: 1,
+        nombre: "Figura Personalizada 5 a 10cm",
+        precio: 15000,
+        imagen: "/assets/img/01.png",
+        categoria: "pequeña",
+    },
+    {
+        id: 2,
+        nombre: "Figura Personalizada 10 a 15cm",
+        precio: 22000,
+        imagen: "/assets/img/02.png",
+        categoria: "mediana",
+    },
+    {
+        id: 3,
+        nombre: "Figura Personalizada 15 a 20cm",
+        precio: 33000,
+        imagen: "/assets/img/03.png",
+        categoria: "grande",
+    },
+];
---
+++ src/data/regions.ts
@@ -0,0 +1,6 @@
+export const regionesYComunas = {
+    "Metropolitana": ["Santiago", "Providencia", "Las Condes", "Vitacura", "La Florida"],
+    "Valparaíso": ["Valparaíso", "Viña del Mar", "Quilpué", "Villa Alemana"],
+    "Biobío": ["Concepción", "Talcahuano", "Chillán", "Los Ángeles"],
+    "Araucanía": ["Temuco", "Padre Las Casas", "Villarrica", "Pucón"]
+};
